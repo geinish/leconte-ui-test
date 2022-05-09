8f6141442b5b3253e63bc1846309f12f801e207c
Гейниш Юлия Андреевна <geinish@gmail.com>
2022-05-09 09:39:12 +0200

    +message for navigation text

--- src/test/java/cases/MainPageNavigationTest.java
+++ src/test/java/cases/MainPageNavigationTest.java
@@ -33,21 +33,21 @@ public class MainPageNavigationTest {
     @Test
     public void shouldBeSpolupraceNavigation() {
         mainPage.navigationSpolupraceClick();
-        Assert.assertEquals("NÁBOR PRACOVNÍKŮ NEBYL NIKDY SNAZŠÍ", mainPage.getSectionSpolupraceText());
-        Assert.assertEquals("https://www.leconte.cz/#spoluprace", mainPage.driver.getCurrentUrl());
+        Assert.assertEquals("When User click SPOLUPRÁCE S NÁMI Then User see the section Spoluprace", "NÁBOR PRACOVNÍKŮ NEBYL NIKDY SNAZŠÍ", mainPage.getSectionSpolupraceText());
+        Assert.assertEquals("When User click SPOLUPRÁCE S NÁMI Then User redirected to #spoluprace","https://www.leconte.cz/#spoluprace", mainPage.driver.getCurrentUrl());
     }
 
     @Test
     public void shouldBeSFormNavigation() {
         mainPage.navigationFormClick();
-        Assert.assertEquals("KONTAKTUJTE NÁS", mainPage.getSectionFormText());
-        Assert.assertEquals("https://www.leconte.cz/#form", mainPage.driver.getCurrentUrl());
+        Assert.assertEquals("When User click KONTAKT Then User see the form for messaging", "KONTAKTUJTE NÁS", mainPage.getSectionFormText());
+        Assert.assertEquals("When User click KONTAKT Then User redirected to #form", "https://www.leconte.cz/#form", mainPage.driver.getCurrentUrl());
     }
 
     @Test
     public void shouldBeSSluzbyNavigation() {
         mainPage.navigationSluzbyClick();
-        Assert.assertEquals("NAŠE SLUŽBY", mainPage.getSectionSluzbyText());
-        Assert.assertEquals("https://www.leconte.cz/#sluzby", mainPage.driver.getCurrentUrl());
+        Assert.assertEquals("When User click O NÁS Then User see the section sluzby", "NAŠE SLUŽBY", mainPage.getSectionSluzbyText());
+        Assert.assertEquals("When User click O NÁS Then User redirected to #sluzby", "https://www.leconte.cz/#sluzby", mainPage.driver.getCurrentUrl());
     }
 }
